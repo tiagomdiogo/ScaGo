4fc707d844a53c27e57c352c7a60c6a21177a5aa
Diniz Sa <diniz.sa@MPT-02182.local>
2022-10-22 18:49:56 +0100

    Simple Sniffer working

--- go.mod
+++ go.mod
@@ -4,4 +4,7 @@ go 1.19
 
 require golang.org/x/net v0.0.0-20220923203811-8be639271d50
 
-require golang.org/x/sys v0.0.0-20220728004956-3c1f35247d10 // indirect
+require (
+	github.com/google/gopacket v1.1.19 // indirect
+	golang.org/x/sys v0.0.0-20220728004956-3c1f35247d10 // indirect
+)
--- main.go
+++ main.go
@@ -12,13 +12,18 @@ func main(){
 	fmt.Println("Welcome to Go Sniffer. To get help in usage please use the -help argument.")
 	args := os.Args[1:]
 
-	if args[0] == "-help" {
-		fmt.Println("Usage:\n go run main.go -i <interface_name> -o <Outputfile> -f <Filters>\n\nNote that -o and -f flags are optional.")
+	if len(args) != 0{
+		if args[0] == "-help"{
+			fmt.Println("Usage:\n go run main.go -i <interface_name> -o <Outputfile> -f <Filters>\n\nNote that -o and -f flags are optional.")
+		}
+		if args[0] == "-i"{
+			interface_name := args[1]
+			sniffer.CapturePacket(interface_name)
+		}
 	}
 
 	//interface_name := args[1]
 	//var output_file String
-
-	fmt.Println(sniffer.Greet1())
+	
 	
 }
