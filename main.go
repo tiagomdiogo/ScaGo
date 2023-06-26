c87f90d4cd4da14a8494f6d86aee29cda2cd30d2
tiago.diogo <tiago.m.diogo@tecnico.ulisboa.pt>
2023-06-26 10:52:00 +0100

    ammend main

--- main.go
+++ main.go
@@ -1,28 +1,20 @@
-package GoPpy
+package goppy
 
-import (
-	"bufio"
-	"fmt"
-	"log"
-	"os"
-	"strings"
-
-	sniffer "github.com/tiagomdiogo/GoPpy/sniffer"
-)
-
+/*
 func execFunc(cmd string) {
 	cmdString := strings.TrimSuffix(cmd, "\n")
 	arrCmd := strings.Fields(cmdString)
 
 	switch arrCmd[0] {
 	case "sniffer":
-		interface_name := arrCmd[1]
-		sniffer.CapturePacket(interface_name)
+		//interface_name := arrCmd[1]
+		//sniffer.CapturePacket(interface_name)
 	case "send":
 	}
 }
 
-func main() {
+/*
+func goppy() {
 
 	fmt.Println("Welcome to Go Sniffer. To get help in usage please use the -help argument.")
 
@@ -41,3 +33,5 @@ func main() {
 	}
 
 }
+
+*/
